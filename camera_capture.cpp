f9d7da3bd000c7f5be5e80ee8f0f33a0a9f5dc2d
Denis Lachance <dudeofea@hotmail.com>
2015-09-14 06:59:41 -0600

    tried to add a hue scale to debug better, refatoring

--- camera_capture.cpp
+++ camera_capture.cpp
@@ -16,10 +16,11 @@ CascadeClassifier face_cascade;
 #define EYE_HEIGHT 		0.30
 #define EYE_WIDTH 		0.35
 #define EYE_RESIZE_WIDTH	50	//size to resize eye box for speed
-#define EYE_GRADIENT_THRESH	50.0
+#define EYE_GRADIENT_THRESH	25.0
 #define EYE_BLUR_SIZE	5
 
 void getGazePosition(Mat &frame);
+Mat toHueScale(Mat img);
 
 int main() {
 	//get camera stream
@@ -62,8 +63,73 @@ int main() {
 	return 0;
 }
 
-void getEyeVectors(Mat frame_gray, Rect face);
-Point getEyeCenter(Mat face, Rect eye);
+Mat toHueScale(Mat img){
+	double h = 0.0, s = 0.5, v = 0.5, ff, p, q, t;
+	//make color array
+	std::vector<cv::Mat> rgb_mat;
+	rgb_mat.push_back(Mat::zeros(img.rows, img.cols, CV_64F));
+	rgb_mat.push_back(Mat::zeros(img.rows, img.cols, CV_64F));
+	rgb_mat.push_back(Mat::zeros(img.rows, img.cols, CV_64F));
+	for (int i = 0; i < img.rows; ++i)
+	{
+		uchar* img_p = img.ptr<uchar>(i);
+		uchar* r_mat = rgb_mat[0].ptr<uchar>(i);
+		uchar* g_mat = rgb_mat[1].ptr<uchar>(i);
+		uchar* b_mat = rgb_mat[2].ptr<uchar>(i);
+		for (int j = 0; j < img.cols; ++j)
+		{
+			//Turn brightness into hue
+			h = 360.0 * img_p[j]/255.0;
+			//convert to RGB
+			h /= 60.0;
+			long k = (long)h;
+			ff = h - k;
+			p = v * (1.0 - s);
+			q = v * (1.0 - (s * ff));
+			t = v * (1.0 - (s * (1.0 - ff)));
+			r_mat[j] = (double)img_p[j];
+			// switch(i) {
+			// case 0:
+			// 	r_mat[j] = v;
+			// 	g_mat[j] = t;
+			// 	b_mat[j] = p;
+			// break;
+			// case 1:
+			// 	r_mat[j] = q;
+			// 	g_mat[j] = v;
+			// 	b_mat[j] = p;
+			// break;
+			// case 2:
+			// 	r_mat[j] = p;
+			// 	g_mat[j] = v;
+			// 	b_mat[j] = t;
+			// break;
+			// case 3:
+			// 	r_mat[j] = p;
+			// 	g_mat[j] = q;
+			// 	b_mat[j] = v;
+			// break;
+			// case 4:
+			// 	r_mat[j] = t;
+			// 	g_mat[j] = p;
+			// 	b_mat[j] = v;
+			// break;
+			// case 5:
+			// default:
+			// 	r_mat[j] = v;
+			// 	g_mat[j] = p;
+			// 	b_mat[j] = q;
+			// break;
+			// }
+		}
+	}
+	Mat color;
+	merge(rgb_mat, color);
+	return color;
+}
+
+void getEyeVectors(Mat &frame_gray, Rect face);
+Point getEyeCenter(Mat &face, Rect eye);
 
 //returns a 2D vector of where the eyes are pointing
 void getGazePosition(Mat &frame){
@@ -91,7 +157,7 @@ void getGazePosition(Mat &frame){
 	//imshow("cam", frame);
 }
 
-void getEyeVectors(Mat frame_gray, Rect face) {
+void getEyeVectors(Mat &frame_gray, Rect face) {
 	Mat face_frame = frame_gray(face);
 
 	//blur to remove some noise
@@ -116,14 +182,14 @@ void getEyeVectors(Mat frame_gray, Rect face) {
 
 	//-- Find Eye Centers
 	Point left_pupil = getEyeCenter(face_frame, left_eye_box);
-	Point right_pupil = getEyeCenter(face_frame, right_eye_box);
-	right_pupil.x += right_eye_box.x;
-	right_pupil.y += right_eye_box.y;
-	left_pupil.x += left_eye_box.x;
-	left_pupil.y += left_eye_box.y;
-	// draw eye centers
-	circle(face_frame, right_pupil, 3, 1234);
-	circle(face_frame, left_pupil, 3, 1234);
+	// Point right_pupil = getEyeCenter(face_frame, right_eye_box);
+	// right_pupil.x += right_eye_box.x;
+	// right_pupil.y += right_eye_box.y;
+	// left_pupil.x += left_eye_box.x;
+	// left_pupil.y += left_eye_box.y;
+	// // draw eye centers
+	// circle(face_frame, right_pupil, 3, 1234);
+	// circle(face_frame, left_pupil, 3, 1234);
 
 	// //-- Find Eye Corners
 	// if (kEnableEyeCorner) {
@@ -145,7 +211,7 @@ void getEyeVectors(Mat frame_gray, Rect face) {
 	// 	circle(face_frame, rightRightCorner, 3, 200);
 	// }
 
-	imshow("cam", face_frame);
+	//imshow("cam", face_frame);
 	//  Rect roi( Point( 0, 0 ), face_frame.size());
 	//  Mat destinationROI = debugImage( roi );
 	//  face_frame.copyTo( destinationROI );
@@ -156,14 +222,14 @@ Mat computeGradient(const Mat &mat){
 	Mat out(mat.rows,mat.cols,CV_64F);
 	for (int y = 0; y < mat.rows; ++y) {
 		const uchar *Mr = mat.ptr<uchar>(y);
-		double *Or = out.ptr<double>(y);
+		double *Or = out.ptr<double>(y);		
 		Or[0] = Mr[1] - Mr[0];
 		for (int x = 1; x < mat.cols - 1; ++x) {
-			Or[x] = (Mr[x+1] - Mr[x-1])/2.0;
+			Or[x] = (Mr[x+1] - Mr[x-1]);
 		}
 		Or[mat.cols-1] = Mr[mat.cols-1] - Mr[mat.cols-2];
 	}
-	return out;
+	return out/2.0;
 }
 
 //returns a matrix of magnitudes given a matrix of x and y values
@@ -181,7 +247,7 @@ Mat matrixMagnitude(Mat &matX, Mat &matY) {
 	return mags;
 }
 
-Point getEyeCenter(Mat face, Rect eye){
+Point getEyeCenter(Mat &face, Rect eye){
 	Mat eye_box = face(eye);
 	
 	//scale down for speed
@@ -199,16 +265,17 @@ Point getEyeCenter(Mat face, Rect eye){
 	
 	//normalize and threshold the gradient
 	Mat mags = matrixMagnitude(gradientX, gradientY);
+
 	//compute the threshold
 	Scalar stdMagnGrad, meanMagnGrad;
 	meanStdDev(mags, meanMagnGrad, stdMagnGrad);
 	double stdDev = stdMagnGrad[0] / sqrt(mags.rows*mags.cols);
 	double gradientThresh = EYE_GRADIENT_THRESH * stdDev + meanMagnGrad[0];
 	
-	//normalize
+	//normalize and threshold
 	for (int y = 0; y < eye_box.rows; ++y) {
 		double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
-		const double *Mr = mags.ptr<double>(y);
+		double *Mr = mags.ptr<double>(y);
 		for (int x = 0; x < eye_box.cols; ++x) {
 			double gX = Xr[x], gY = Yr[x];
 			double magnitude = Mr[x];
@@ -218,9 +285,11 @@ Point getEyeCenter(Mat face, Rect eye){
 			} else {
 				Xr[x] = 0.0;
 				Yr[x] = 0.0;
+				Mr[x] = 0.0;
 			}
 		}
 	}
+
 	//-- Create a blurred and inverted image for weighting
 	Mat weight;
 	GaussianBlur(eye_box, weight, Size(EYE_BLUR_SIZE, EYE_BLUR_SIZE), 0, 0);
@@ -230,7 +299,7 @@ Point getEyeCenter(Mat face, Rect eye){
 			row[x] = (255 - row[x]);
 		}
 	}
-	//imshow("cam", weight);
+	imshow("cam", toHueScale(weight));
 	//-- Run the algorithm!
 	Mat outSum = Mat::zeros(eye_box.rows,eye_box.cols,CV_64F);
 	// for each possible gradient location
@@ -262,7 +331,7 @@ Point getEyeCenter(Mat face, Rect eye){
 					double dotProduct = dx*gY + dy*gY;
 					dotProduct = std::max(0.0,dotProduct);
 					// square and multiply by the weight
-					Or[cx] += dotProduct * dotProduct * (Wr[cx]/20.0);
+					Or[cx] += dotProduct * dotProduct * (Wr[cx]/1.0);
 				}
 			}
 		}
@@ -275,23 +344,6 @@ Point getEyeCenter(Mat face, Rect eye){
 	Point maxP;
 	double maxVal;
 	minMaxLoc(out, NULL,&maxVal,NULL,&maxP);
-	
-	// //-- Flood fill the edges
-	// if(kEnablePostProcess) {
-	// 	Mat floodClone;
-	// 	//double floodThresh = computeDynamicThreshold(out, 1.5);
-	// 	double floodThresh = maxVal * kPostProcessThreshold;
-	// 	threshold(out, floodClone, floodThresh, 0.0f, THRESH_TOZERO);
-	// 	if(kPlotVectorField) {
-	// 		//plotVecField(gradientX, gradientY, floodClone);
-	// 		imwrite("eyeFrame.png",eye_boxUnscaled);
-	// 	}
-	// 	Mat mask = floodKillEdges(floodClone);
-	// 	//imshow(debugWindow + " Mask",mask);
-	// 	//imshow(debugWindow,out);
-	// 	// redo max
-	// 	minMaxLoc(out, NULL,&maxVal,NULL,&maxP,mask);
-	// }
-	// return unscalePoint(maxP,eye);
-	return maxP;
+	circle(eye_box, maxP, 3, 200);
+	// return maxP;
 }
